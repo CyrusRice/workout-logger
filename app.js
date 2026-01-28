2ffbbdc078e83f15ae6e7e5ee3524967569b02ca
Cyrus Rice <cyrus.rice@frontier.com>
2026-01-28 12:54:48 -0800

    Final code cleanup, misc style changes

--- app.js
+++ app.js
@@ -16,49 +16,10 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-/* Mongoose routes
-app.get('/add-workout', (req, res) => {
-  const workout = new Workout({
-    date: '11-01-2025',
-    time: '1 h 5 m',
-    notes: 'buycep curlzz'
-  });
-
-  Workout.save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-})
-
-app.get('/all-blogs', (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-})
-    */
-
 // Home page
 app.get('/', (req, res) => {
-  /*const workouts = [
-    {date: '11-1-2025', time: '1:05', notes: 'bicep curls 3x10, 30 min treadmill'},
-    {date: '11/5/2025', time: '1 h 12 m', notes: ''},
-    {date: '11/10/25', time: '57 min', notes: ''}
-  ];
-  res.render('index', { title: 'Home', workouts });*/
   res.redirect('/workouts');
 });
-
-/* Add a workout
-app.get('/workouts/add', (req, res) => {
-  res.render('add', { title: 'Add a new workout' });
-});*/
 
 // Workout routes
 app.use('/workouts', workoutRoutes);
